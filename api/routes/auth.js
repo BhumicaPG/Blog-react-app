f768965c86f6c9a1add9dbdc074aaea825c77907
BhumicaPG <bhumigangurde8@gmail.com>
2023-07-15 00:14:36 +0530

    Added files

---
+++ api/routes/auth.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPass,
+    });
+
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username }); //findone function is for finding that "ONLY ONE UNIQUE" user in mongodb
+    !user && res.status(400).json("Wrong credentials!"); //if not found
+
+    const validated = await bcrypt.compare(req.body.password, user.password); //for comparing the currently entered password by the user inside my mongodb
+    !validated && res.status(400).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
